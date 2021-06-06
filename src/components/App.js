738ce53dccc2efc7f9eedaf0c14d54ab17819752
OceanMud <oceanmudhs@gmail.com>
2021-06-05 19:12:42 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-xl font-bold ">hi</div>
+    </div>
+  );
+}
+
+export default App;
